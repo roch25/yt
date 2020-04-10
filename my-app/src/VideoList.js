158e16a519f56f4d0df2ef87770c9cc880578d7b
git-roch <rochellemartins25@gmail.com>
2020-04-10 20:45:49 +0530

    commit

--- my-app/src/VideoList.js
+++ my-app/src/VideoList.js
@@ -1,5 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactPlayer from 'react-player';
+
+function VideoPlayer(props){
+ var vid_id = props.vid_id;
+ var videoUrl = "https://www.youtube.com/watch?v=" + vid_id;
+ console.log(videoUrl);
+  return (
+    <div id = "player">
+      <ReactPlayer id = "c" url = {videoUrl}/>
+    </div>
+  )
+}
+
+function play_video(vidId){
+  console.log('boop '+vidId);
+  ReactDOM.render(
+    <VideoPlayer vid_id = {vidId}/>,
+    document.getElementById('search-result-box')
+    // window.open();
+  );
+}
+
+function ListItems(props){
+  const a_list = props.li;
+  const items =  a_list.map((a_list) =>
+  <li id = 'li1' key = {a_list.snippet.title} onClick = {() => play_video(a_list.id.videoId)}>
+  <img src={a_list.snippet.thumbnails.default.url} id="thumbnail" alt="No Image Available" />
+   <div id = "li2">
+     <p id = "title">{a_list.snippet.title}</p>
+     <p id = "desc">{a_list.snippet.description}</p>
+   </div>
+  </li>)
+  return (
+    <div>{items}</div>
+  )
+}
+
+function VideoList(props) {
+  return (
+    <ul>
+    <ListItems li = {props.a_list}/>
+    </ul>
+  )
+}
+
+export default VideoList;
+
 
 // function ImageList(props){
 //   const t_img = props.image_list;
@@ -50,31 +97,3 @@ import ReactDOM from 'react-dom';
 //     </li>
 //   )
 // }
-function play_video(){
-  console.log('boop');
-}
-
-function ListItems(props){
-  const a_list = props.li;
-  const items =  a_list.map((a_list) =>
-  <li id = 'li1' key = {a_list.snippet.title} onClick = {play_video}>
-  <img src={a_list.snippet.thumbnails.default.url} id="thumbnail" alt="No Image Available" />
-   <div id = "li2">
-     <p id = "title">{a_list.snippet.title}</p>
-     <p id = "desc">{a_list.snippet.description}</p>
-   </div>
-  </li>)
-  return (
-    <div>{items}</div>
-  )
-}
-
-function VideoList(props) {
-  return (
-    <ul>
-    <ListItems li = {props.a_list}/>
-    </ul>
-  )
-}
-
-export default VideoList;
