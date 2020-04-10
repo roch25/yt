3c3fad2a9320bfdd36b6a5f98521908cee64d941
git-roch <rochellemartins25@gmail.com>
2020-04-11 01:37:52 +0530

    commit 11/4

--- my-app/src/VideoList.js
+++ my-app/src/VideoList.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactPlayer from 'react-player';
-
-function VideoPlayer(props){
- var vid_id = props.vid_id;
- var desc = props.desc;
- var title = props.title;
- var videoUrl = "https://www.youtube.com/watch?v=" + vid_id;
- console.log(videoUrl);
-  return (
-    <div id = "player">
-      <ReactPlayer url = {videoUrl}/>
-      <p id = 'p1'>{title}</p>
-      <p>{desc}</p>
-    </div>
-  )
-}
+import VideoPlayer from './VideoPlayer'
 
 function play_video(vidId, title, desc){
   console.log('boop '+vidId);
