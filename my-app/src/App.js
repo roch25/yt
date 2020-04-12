12ef849c5ba99f29a47cd226c436df7876d0d07c
git-roch <rochellemartins25@gmail.com>
2020-04-12 22:07:52 +0530

    commit 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,30 +5,31 @@ import './App.css';
 import fetch_search_list from './Request';
 import VideoList from './VideoList';
 
-function display_search_list(){
-  fetch_search_list().then(res => {
-    console.log(res);
-    ReactDOM.render(
-      <VideoList a_list = {res}/>,
-      document.getElementById('search-result-box')
-    );
-  })
-}
+class App extends React.Component{
+  display_search_list(){
+    fetch_search_list().then(res => {
+      ReactDOM.render(
+        <VideoList a_list = {res}/>,
+        document.getElementById('search-result-box')
+      );
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
+  render(){
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className = "Search">
-        <input type = "text" id = "search-box" placeholder = "Search"/>
-         &nbsp;
-        <input className = "search-button" type = "button" onClick = {display_search_list} src = "search.png"/>
-        </div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className = "Search">
+      <input type = "text" id = "search-box" placeholder = "Search"/>
+      &nbsp;
+      <input className = "search-button" type = "button" onClick = {this.display_search_list} src = "search.png"/>
+      </div>
       </header>
       <div id="search-result-box"></div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
