5005a7f4463ce54a96d3da3d44773ef4348d9558
git-roch <rochellemartins25@gmail.com>
2020-04-13 01:07:31 +0530

    commit 4

--- my-app/src/ListItems.js
+++ my-app/src/ListItems.js
@@ -6,14 +6,14 @@ class ListItems extends React.Component{
   play_video(vidId, title, desc){
     console.log(vidId);
     ReactDOM.render(
-      <VideoPlayer vid_id = {vidId} title = {title} desc = {desc}/>,
+      <VideoPlayer vid_id = {vidId} title = {title} desc = {desc} />,
       document.getElementById('search-result-box')
     );
   }
   render(){
     const a_list = this.props.li;
     const items =  a_list.map((a_list) =>
-    <li id = 'li1' key = {a_list.snippet.title} onClick = {() => this.play_video(a_list.id.videoId,a_list.snippet.title, a_list.snippet.description)}>
+    <li id = 'li1' key = {a_list.id.videoId} onClick = {() => this.play_video(a_list.id.videoId,a_list.snippet.title, a_list.snippet.description)}>
     <img src={a_list.snippet.thumbnails.default.url} id="thumbnail" alt="No Image Available" />
      <div id = "li2">
        <p id = "title">{a_list.snippet.title}</p>
